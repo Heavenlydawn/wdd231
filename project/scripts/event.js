a714d8a5972dfcb8518cf8357f5780383494d4c6
Ozioma Heaven Gabriel <gabrielheavenlydawn@gmail.com>
2025-06-16 14:50:47 +0100

    fix: remove unused normalize.css link from multiple HTML files for consistency

--- project/scripts/event.js
+++ project/scripts/event.js
@@ -1,54 +1,58 @@
-const displayAllEvents = document.querySelector('.dsAllEvents');
-const moreInfoWrapper = document.querySelector('.moreInfoWrapper');
+const displayAllEvents = document.querySelector(".dsAllEvents");
+const moreInfoWrapper = document.querySelector(".moreInfoWrapper");
 
-const key = 'SGAsgKjftKVJvlQdkQCsXnLgAukjz16E';
+const key = "SGAsgKjftKVJvlQdkQCsXnLgAukjz16E";
 const url = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&stateCode=ON&size=20&apikey=${key}`;
 
 async function fetchCanadaEventData() {
-    try {
-        const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-
-        const getData = await response.json();
-        // console.log(getData);
-        const data = getData._embedded.events;
-
-        displayEvents(data);
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
+
+    const getData = await response.json();
+    // console.log(getData);
+    const data = getData._embedded.events;
+
+    displayEvents(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 fetchCanadaEventData();
 
-
 // Display all events
 function displayEvents(data) {
-    displayAllEvents.innerHTML = '';
+  displayAllEvents.innerHTML = "";
 
-    data.forEach(event => {
-        const eventItem = document.createElement('div');
-        eventItem.classList.add('eachItem');
+  data.forEach((event) => {
+    const eventItem = document.createElement("div");
+    eventItem.classList.add("eachItem");
 
-        // Event structure
-        eventItem.innerHTML = `
+    // Event structure
+    eventItem.innerHTML = `
             <div> 
-                <img src="${event.images[3].url}" alt="${event.name}" width="${event.images[3].width}" height="${event.images[3].height}" loading="lazy"> 
+                <img src="${event.images[3].url}" alt="${event.name}" width="${
+      event.images[3].width
+    }" height="${event.images[3].height}" loading="lazy"> 
             </div>
 
             <ul>
                 <li><strong>${event.name}</strong></li>
-                <li>${new Date(event.dates.start.dateTime).toLocaleString('en-US', {
+                <li>${new Date(event.dates.start.dateTime).toLocaleString(
+                  "en-US",
+                  {
                     month: "short",
                     day: "2-digit",
                     year: "numeric",
                     hour: "numeric",
                     minute: "2-digit",
-                    hour12: true
-                })}</li>
+                    hour12: true,
+                  }
+                )}</li>
 
                 <li>
                     ${event._embedded.venues[0].name}, 
@@ -58,24 +62,27 @@ function displayEvents(data) {
             </ul>
         `;
 
-        // Dialog information when clicked
-        eventItem.addEventListener('click', () => {
-
-            moreInfoWrapper.innerHTML = `
+    // Dialog information when clicked
+    eventItem.addEventListener("click", () => {
+      moreInfoWrapper.innerHTML = `
 
                 <span class="closeBtn" autofocus>❌</span>
 
                 <h2>${event.name}</h2>
-                <p><strong>Important Event Info</strong> <br> ${event.pleaseNote || 'No additional information available.'}</p>
+                <p><strong>Important Event Info</strong> <br> ${
+                  event.pleaseNote || "No additional information available."
+                }</p>
                                 
                 <div class="timeVenue">
-                    <p><strong>Date & Time:</strong> ${new Date(event.dates.start.dateTime).toLocaleString('en-US', {
-                        month: "short",
-                        day: "2-digit",
-                        year: "numeric",
-                        hour: "numeric",
-                        minute: "2-digit",
-                        hour12: true
+                    <p><strong>Date & Time:</strong> ${new Date(
+                      event.dates.start.dateTime
+                    ).toLocaleString("en-US", {
+                      month: "short",
+                      day: "2-digit",
+                      year: "numeric",
+                      hour: "numeric",
+                      minute: "2-digit",
+                      hour12: true,
                     })}</p>
 
                     <p><strong>Venue:</strong> 
@@ -91,15 +98,15 @@ function displayEvents(data) {
 
             `;
 
-            // Close dialog
-            const closeBtn = moreInfoWrapper.querySelector('.closeBtn');
-            closeBtn.addEventListener('click', () => {
-                moreInfoWrapper.close();
-            });
+      // Close dialog
+      const closeBtn = moreInfoWrapper.querySelector(".closeBtn");
+      closeBtn.addEventListener("click", () => {
+        moreInfoWrapper.close();
+      });
 
-            moreInfoWrapper.showModal(); // Show dialog
-        });
-
-        displayAllEvents.appendChild(eventItem);
+      moreInfoWrapper.showModal(); // Show dialog
     });
+
+    displayAllEvents.appendChild(eventItem);
+  });
 }
